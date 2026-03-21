f83e87f1ddfd01658c27294d37abffe8776e0025
Subhojitdrm <subhojitroypf@gmail.com>
2026-03-21 14:07:55 +0530

    Fix survey insert placeholder count

--- apps/api/src/modules/surveys/surveys.repository.js
+++ apps/api/src/modules/surveys/surveys.repository.js
@@ -20,7 +20,7 @@ export const surveysRepository = {
           willingness_to_pay,
           monthly_price_range
         )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
         RETURNING
           id,
           name,
